d23008fab3e5db5925d8856683f0918fcb676ae8
Marcilio <prolaser2005hotmail.com>
2026-07-25 21:15:30 -0300

    feat(api): adiciona ligas europeias (Premier League, La Liga, Serie A, Bundesliga, Ligue 1) ao monitoramento 2026

--- apps/api/src/config/competitions.config.ts
+++ apps/api/src/config/competitions.config.ts
@@ -80,6 +80,46 @@ export const SUPPORTED_COMPETITIONS: CompetitionConfig[] = [
     type: 'league',
     activeSeasons: [2026],
   },
+  {
+    code: 'ENG_PREMIER_LEAGUE',
+    externalId: 39,
+    name: 'Premier League',
+    country: 'England',
+    type: 'league',
+    activeSeasons: [2026],
+  },
+  {
+    code: 'ESP_LA_LIGA',
+    externalId: 140,
+    name: 'La Liga',
+    country: 'Spain',
+    type: 'league',
+    activeSeasons: [2026],
+  },
+  {
+    code: 'ITA_SERIE_A',
+    externalId: 135,
+    name: 'Serie A',
+    country: 'Italy',
+    type: 'league',
+    activeSeasons: [2026],
+  },
+  {
+    code: 'GER_BUNDESLIGA',
+    externalId: 78,
+    name: 'Bundesliga',
+    country: 'Germany',
+    type: 'league',
+    activeSeasons: [2026],
+  },
+  {
+    code: 'FRA_LIGUE_1',
+    externalId: 61,
+    name: 'Ligue 1',
+    country: 'France',
+    type: 'league',
+    activeSeasons: [2026],
+  },
 ];
 
 export const DEFAULT_COMPETITION = {
